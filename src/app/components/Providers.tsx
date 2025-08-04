6078ac6d683ddaa533738278d06553fa8cfeeb59
yashm13114 <yashm13114@gmail.com>
2025-08-04 13:55:11 -0400

    commented providers

--- src/app/components/Providers.tsx
+++ src/app/components/Providers.tsx
@@ -1,22 +1,22 @@
-'use client';
+// 'use client';
 
-import {PrivyProvider} from '@privy-io/react-auth';
+// import {PrivyProvider} from '@privy-io/react-auth';
 
-export default function Providers({children}: {children: React.ReactNode}) {
-  return (
-    <PrivyProvider
-      appId="cmdsv5qxy007wky0ah8l7uhs7"
-      clientId="client-WY6NyHoFmBtsAVGMZJcgDzviFMq9KAPv3SUckXgRQKSDw"
-      config={{
-        // Create embedded wallets for users who don't have a wallet
-        embeddedWallets: {
-          solana: {
-            createOnLogin: 'users-without-wallets'
-          }
-        }
-      }}
-    >
-      {children}
-    </PrivyProvider>
-  );
-}
+// export default function Providers({children}: {children: React.ReactNode}) {
+//   return (
+//     <PrivyProvider
+//       appId="cmdsv5qxy007wky0ah8l7uhs7"
+//       clientId="client-WY6NyHoFmBtsAVGMZJcgDzviFMq9KAPv3SUckXgRQKSDw"
+//       config={{
+//         // Create embedded wallets for users who don't have a wallet
+//         embeddedWallets: {
+//           solana: {
+//             createOnLogin: 'users-without-wallets'
+//           }
+//         }
+//       }}
+//     >
+//       {children}
+//     </PrivyProvider>
+//   );
+// }
